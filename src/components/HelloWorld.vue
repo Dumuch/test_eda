88d0e12e935732d722429e4ae6f39e9b3ba51685
Dumuch <64353295+Dumuch@users.noreply.github.com>
2022-02-19 21:41:14 +0800

    summary

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,195 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="body">
+    <header class="header">Тест</header>
+    <main class="container">
+      <aside class="sidebar">
+        <form action="" class="form">
+          <label for="product-title" class="title form_title"
+            >Выберите продукцию</label
+          >
+          <v-select
+            v-model="form.selected"
+            id="product-title"
+            label="title"
+            :options="productList"
+            class="input form_input title--blue"
+          ></v-select>
+
+          <label for="product-quantity" class="title form_title">
+            Введите количество
+          </label>
+          <input
+            type="number"
+            id="product-quantity"
+            min="1"
+            v-model="form.quantity"
+            required
+            class="input form_input title--blue"
+          />
+          <button
+            class="button form_button button--blue"
+            @click.prevent="addCart"
+          >
+            Добавить
+          </button>
+        </form>
+      </aside>
+
+      <section class="content">
+        <table class="table content__table w-100">
+          <tbody>
+            <tr class="d-flex mb-6 d-sm-none">
+              <td class="title title--blue flex-grow">Название товара</td>
+              <td
+                class="
+                  title title--blue
+                  w-20
+                  title--align-right
+                  min-fit-content
+                "
+              >
+                Количество
+              </td>
+              <td
+                class="
+                  title title--blue
+                  w-20
+                  title--align-right
+                  min-fit-content
+                "
+              >
+                Стоимость
+              </td>
+            </tr>
+            <tr
+              class="d-flex mb-3 flex-sm-wrap"
+              v-for="item in cart"
+              :key="item.id"
+            >
+              <td class="title flex-grow w-sm-100">
+                {{ item.title }}
+              </td>
+              <td
+                class="
+                  title
+                  w-20
+                  title--align-right title--align-sm-left
+                  min-fit-content
+                  w-sm-50
+                "
+              >
+                {{ item.quantity }} шт.
+              </td>
+              <td class="title w-20 title--align-right w-sm-50 min-fit-content">
+                {{ item.price.toFixed(3) }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div class="title w-100 title--align-right mt-6">
+          <span> <span class="d-sm-none">Итого: </span>{{ totalPrice }}</span>
+        </div>
+        <div class="d-flex justify-center mt-6">
+          <button
+            class="button button--green"
+            @click.prevent="saveCart"
+            :disabled="isDisabledButtonSave"
+          >
+            Сохранить
+          </button>
+        </div>
+
+        <div class="d-flex justify-center mt-6" v-if="orderNumber">
+          <span class="title">Ваш номер заказа: {{ orderNumber }}</span>
+        </div>
+      </section>
+    </main>
+
+    <footer class="footer d-flex flex-column">
+      <div class="d-flex justify-end mt-a flex-grow">
+        <span class="title footer__title title--white title--align-right"
+          >Тест</span
+        >
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  title: "HelloWorld",
+  data() {
+    return {
+      productList: [],
+      cart: [],
+      form: {
+        selected: null,
+        quantity: null,
+      },
+      totalPrice: 0,
+      orderNumber: null,
+    };
+  },
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  async mounted() {
+    const data = await fetch(
+      "https://dev-su.eda1.ru/test_task/products.php"
+    ).then((r) => r.json());
+    this.productList = data.products;
+  },
+
+  methods: {
+    addCart() {
+      if (this.form.quantity > 0) {
+        const product = this.productList.find(
+          (item) => item.id === this.form.selected.id
+        );
+        this.cart.push({
+          id: Date.now(),
+          title: product.title,
+          quantity: this.form.quantity,
+          price: product.price * this.form.quantity,
+        });
+
+        this.form.selected = null;
+        this.form.quantity = null;
+      }
+    },
+
+    async saveCart() {
+      const data = await fetch("https://dev-su.eda1.ru/test_task/save.php", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(this.cart),
+      })
+        .then((r) => r.json())
+        .catch((e) => console.log(e.json()));
+
+      this.orderNumber = data.code;
+    },
+  },
+
+  computed: {
+    isDisabledButtonSave() {
+      if (this.cart.length > 0) {
+        return false;
+      } else {
+        return true;
+      }
+    },
+  },
+
+  watch: {
+    cart() {
+      const totalPrice = this.cart.reduce((acc, item) => {
+        return acc + item.price;
+      }, null);
+
+      this.totalPrice = totalPrice.toFixed(2);
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vSelect from "vue-select";
 
-Vue.config.productionTip = false
+Vue.component("v-select", vSelect);
+import "vue-select/dist/vue-select.css";
+
+import "./assets/style.css";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
